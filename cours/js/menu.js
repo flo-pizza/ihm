4b1e5db428ffa9979d826384a30337f16b6a021f
FL <halfpaperboy@gmail.com>
2025-04-20 23:49:25 +0200

    v1 site ihm

---
+++ cours/js/menu.js
@@ -0,0 +1,19 @@
+function writeMenus() {
+
+    topMenus = {
+        "Accueil" : "/~mountaz/Ens/Ihm/",
+        //"Technotes": "/~mountaz/Ens/Ihm/Technotes/",
+        "Videos": "/~mountaz/Ens/Ihm/Videos/",
+        "Biblio": "/~mountaz/Ens/Ihm/Papers/",
+        "Cours": "/~mountaz/Ens/Ihm/Cours/"
+
+    };
+
+    document.write("<div id=\"menu\">\n<ul>\n<li style=\"border-right: 1px solid #d0d0d0;\">\n");
+    for(var title in topMenus){
+        document.write("<li style=\"border-right: 1px solid #d0d0d0;\"><a href=\""+topMenus[title]+"\">" + title + "</a>");
+    };
+    document.write("</ul>\n</div>");
+}
+
+
---
+++ js/theme.js
@@ -0,0 +1,33 @@
+console.log("Script chargé !");
+
+/* fonctionnement bouton jour/nuit */
+const themeToggle = document.getElementById('themeToggle');
+
+// Appliquer le thème sauvegardé
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+    themeToggle.checked = true;
+}
+
+// Réagir au clic sur le switch
+themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+        document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+
+    // Appliquer la police choisie (si elle existe en localStorage)
+    document.addEventListener("DOMContentLoaded", () => {
+        const savedFont = localStorage.getItem("fontFamily");
+        if (savedFont) {
+            document.body.style.fontFamily = `'${savedFont}', sans-serif`;
+        }
+    });
+
+});
+
+
+
